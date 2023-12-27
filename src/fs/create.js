7dd6b7d97dd6e300269ed9455d308141029a7f30
aleksem07 <aleksem07@gmail.com>
2023-12-27 17:39:58 +0300

    feat: add fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+
+const path = 'src/fs/files/fresh.txt';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.promises.access(path, fs.constants.F_OK);
+    console.log('File already exists');
+  } catch {
+    try {
+      fs.writeFile(path, 'I am fresh and young', (err) => {
+        if (err) throw console.error('FS operation failed');
+        console.log('File created');
+      })
+    } catch (err) {
+      console.error(err);
+    }
+  }
 };
 
 await create();
