1fa731c2eb3a788755fc9ed2e98a9cbc68b565c7
aleksem07 <aleksem07@gmail.com>
2024-01-13 23:15:45 +0300

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+
+const WRONG_FILE = 'src/fs/files/wrongFilename.txt';
+const PROPER_FILE = 'src/fs/files/properFilename.txt';
+
 const rename = async () => {
-    // Write your code here 
+    try {
+      if (fs.existsSync(PROPER_FILE) || !fs.existsSync(WRONG_FILE)) {
+        return console.log('FS operation failed');
+      }
+
+      fs.rename(WRONG_FILE, PROPER_FILE, err => {
+        if (err) throw err;
+        console.log('File renamed');
+      })
+    } catch(err) {
+      console.error(err);
+    }
 };
 
 await rename();
