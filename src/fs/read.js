56563db07973e15bb38a4be052fbe36d358b1c2a
aleksem07 <aleksem07@gmail.com>
2024-01-14 00:00:10 +0300

    feat: add read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+
+const PATH = 'src/fs/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+  try{
+    if(!fs.existsSync(PATH)) {
+      throw new Error('FS operation failed');
+    }
+    fs.readFile(PATH, 'utf8', (err, data) => {
+      if (err) throw new Error('FS operation failed');
+      console.log(data);
+    })
+  } catch(err) {
+    throw err;
+  }
 };
 
 await read();
