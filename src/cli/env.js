4f4f66cb2d7b0c4479de6e562c5209b51e3fe8da
aleksem07 <aleksem07@gmail.com>
2024-01-24 00:35:55 +0300

    feat: resolveenv.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
-    // Write your code here 
+  const ENV_RSS_COUNT = 4;
+  const PREFIX = 'RSS_';
+  const env = process.env;
+  
+  for (let i = 0; i < ENV_RSS_COUNT; i++) {
+    env[`${PREFIX}name${i+1}`] = `value${i+1}`;
+  }
+
+  for (let key in env) {
+    if (key.startsWith(PREFIX)) {
+      console.log(`${key} = ${env[key]}`);
+    }
+  }
 };
 
 parseEnv();
