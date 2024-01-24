6b0bb163c5ef22ffc435f16947cf506ae80697c2
aleksem07 <aleksem07@gmail.com>
2024-01-25 00:37:29 +0300

    feat: resolve args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const options = [];
+  const hasValidArguments = args.some(arg => arg.startsWith('--'))
+
+    if (args.length < 2 || !hasValidArguments) {
+    console.error('Invalid number of command line arguments');
+    return;
+  }
+
+  args.map((arg, i, array) => arg.startsWith('--') && 
+                              array[i+1] &&
+                              !array[i+1].startsWith('--') &&
+                              options.push(`${arg} is ${array[i+1]}` || `${arg} is undefined`));
+
+  console.log(options.join('\n') || 'Invalid number of command line arguments');
 };
 
 parseArgs();
