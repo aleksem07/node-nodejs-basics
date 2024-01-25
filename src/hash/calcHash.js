933991b361a377a7f15312e81f60ca5bb5521ef7
aleksem07 <aleksem07@gmail.com>
2024-01-25 22:17:15 +0300

    feat: resolve hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from 'crypto';
+
+const hash = createHash('sha256');
+
 const calculateHash = async () => {
-    // Write your code here 
+  hash.on('readable', () => {
+    const data = hash.read();
+
+    if (data) {
+      console.log(data.toString('hex'));
+    }
+  })
+
+  hash.write('./files/fileToCalculateHashFor.txt');
+  hash.end();
 };
 
 await calculateHash();
